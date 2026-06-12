b6630a47e63a39e5a6e815fd1d98ad10b0e27a1c
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-12 15:56:26 -0300

    Refactor: remove routing logic to AppRouter component

---
+++ src/router/index.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Register } from '../pages/Register';
+import { Login } from '../pages/Login';
+import { Logout } from '../pages/Logout';
+import { Feed } from '../pages/Feed';
+import { BlogPost } from '../pages/BlogPost';
+import { ProtectedRoute } from '../components/ProtectedRoute';
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth">
+                    <Route path="register" element={<Register />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="logout" element={<Logout />}></Route>
+                </Route>
+                <Route path="/">
+                    <Route
+                        path=""
+                        element={
+                            <ProtectedRoute>
+                                <Feed />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="blog-post/:slug"
+                        element={
+                            <ProtectedRoute>
+                                <BlogPost />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
